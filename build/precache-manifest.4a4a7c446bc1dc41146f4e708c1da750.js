57f5f520eb54a4a0c74b59a387dcdf22d525b8a0
BratiPradhan <58934315+BratiPradhan@users.noreply.github.com>
2020-06-03 20:34:26 +0530

    Add files via upload

---
+++ build/precache-manifest.4a4a7c446bc1dc41146f4e708c1da750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2e6df0f68fde1d59fab3a01dd698ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebc033261bf9ec1942ec",
+    "url": "/static/css/main.489d9dfd.chunk.css"
+  },
+  {
+    "revision": "8c363021454f36faf87c",
+    "url": "/static/js/2.42547532.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42547532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc033261bf9ec1942ec",
+    "url": "/static/js/main.ee645783.chunk.js"
+  },
+  {
+    "revision": "ded419595feb73f778e6",
+    "url": "/static/js/runtime-main.23acf746.js"
+  }
+]);
